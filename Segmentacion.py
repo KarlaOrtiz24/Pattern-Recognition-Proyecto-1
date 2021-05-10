eb42939206520f8897ac19d8f7ed5c4400bba802
Starsonic100 <67350212+Starsonic100@users.noreply.github.com>
2021-05-10 00:12:45 -0700

    Insercion manual de valore de filtros y umbral

--- Segmentacion.py
+++ Segmentacion.py
@@ -7,6 +7,12 @@ import cv2
 from PIL import Image
 from skimage import io 
 
+def Umbralizacion(imagen,x):
+    gray = cv2.medianBlur(imagen, 5)
+
+    _, dst1 = cv2.threshold(gray, int(x), 255, cv2.THRESH_BINARY)
+#    cv2.imshow('umbral fijo', dst1)
+    cv2.imwrite('umbral.jpg', dst1)#para guardarla
 
 def Mediana(nombre, valor):
     imagen = cv2.imread(r'C:\Users\sonic\Documents\ESCOM\Octavo Semestre\Pattern Recognition\Pattern-Recognition-Proyecto-1-main\Pattern-Recognition-Proyecto-1-main\CMA-x1.png', 0)
@@ -20,7 +26,7 @@ def Minimo(n,ruta):
     img = cv2.imread(ruta)
 
       #Crea la forma del kernel
-    tam = (n, n)
+    tam = (int(n), int(n))
     forma = cv2.MORPH_RECT
     kernel = cv2.getStructuringElement(forma, tam)
 
@@ -28,28 +34,49 @@ def Minimo(n,ruta):
     imgResult = cv2.erode(img, kernel)
 
       #Muestra el resultado
-    cv2.namedWindow('Resultado con n ' + str(n), cv2.WINDOW_NORMAL
+    cv2.namedWindow('Resultado con n ' + str(n), cv2.WINDOW_NORMAL)
     cv2.imshow('Resultado con n ' + str(n), imgResult)
+    cv2.waitKey(50)
 
 def Maximo(n,ruta):
     img = cv2.imread(ruta)
 
-      # Creates the shape of the kernel
-    tam = (n,n)
+      # Crea la forma del kernel
+    tam = (int(n),int(n))
     forma = cv2.MORPH_RECT
     kernel = cv2.getStructuringElement(forma, tam)
 
-      # Applies the maximum filter with kernel NxN
+      #Aplica el filtro
     imgResult = cv2.dilate(img, kernel)
 
-      # Shows the result
-    cv2.namedWindow('Result with n ' + str(n), cv2.WINDOW_NORMAL) # Adjust the window length
+      #Muestra el resultado
+    cv2.namedWindow('Result with n ' + str(n), cv2.WINDOW_NORMAL) 
     cv2.imshow('Result with n ' + str(n), imgResult)
+    cv2.waitKey(50)
 
 imagen = cv2.imread('CMA-x1.png')
 gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 cv2.imshow('imagen', gris)
 cv2.waitKey(0)
 cv2.imwrite('gris.jpg', gris)#para guardarla
-Maximo(3,'gris.jpg')
-Minimo(5,'gris.jpg')
+continuar=False
+while continuar==False:
+    print("Inserte el valor del filtro para realizar la umbralización")
+    x=input()
+    Umbralizacion(gris,x)
+    print("Inserte el valor para el filtro Maximo, de preferencia un número entre 1 y 10")
+    n=input()
+    Maximo(n,'umbral.jpg')
+    print("Inserte el valor para el filtro Minimo, de preferencia un número entre 1 y 10")
+    m=input()
+    Minimo(m,'umbral.jpg')
+    BN = cv2.imread('umbral.jpg')
+    cv2.imshow('umbral fijo', BN)
+    cv2.waitKey(50)
+    print("Esta satisfecho con el resultado?")
+    respuesta=input()
+    if respuesta=="Y":
+        continuar=True
+    else:
+        continuar=False
+    
